bb74e770db2dd306124a746c4790be71bdfac9f6
Christian Gonçalves <christiangoncalvesr@gmail.com>
2022-01-25 12:50:10 -0300

    chore: setup test coverage for the app

--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,6 @@ module.exports = {
   setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }]
-  }
+  },
+  collectCoverageFrom: ['<rootDir>/src/**/*.+(js|jsx|ts|tsx)']
 }
