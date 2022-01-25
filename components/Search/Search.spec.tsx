26428a1d19bce4b066cdf9931bc4e32167d2aaf2
Christian Gonçalves <christiangoncalvesr@gmail.com>
2022-01-25 11:05:55 -0300

    feat: add search tests

--- components/Search/Search.spec.tsx
+++ components/Search/Search.spec.tsx
@@ -1,10 +1,48 @@
 import Search from './Search'
-import { render, screen } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+
+const doSearch = jest.fn()
 
 describe('Search', () => {
-  it('should render Search component', () => {
-    render(<Search />)
+  it('should render a form', () => {
+    render(<Search doSearch={doSearch} />)
 
-    expect(screen.getByTestId('search')).toBeInTheDocument()
+    expect(screen.getByRole('form')).toBeInTheDocument()
+  })
+
+  it('should render a input with type equals search', () => {
+    render(<Search doSearch={doSearch} />)
+
+    expect(screen.getByRole('searchbox')).toHaveProperty('type', 'search')
+  })
+
+  it('should render a input with placeholder equals "Search watch"', () => {
+    render(<Search doSearch={doSearch} />)
+
+    expect(screen.getByRole('searchbox')).toHaveProperty('placeholder', 'Search watch')
+  })
+
+  it('should call prop doSearch when form is submitted', () => {
+    render(<Search doSearch={doSearch} />)
+
+    const form = screen.getByRole('form')
+
+    fireEvent.submit(form)
+
+    expect(doSearch).toHaveBeenCalledTimes(1)
+  })
+
+  it('should call prop doSearch with the user input', () => {
+    render(<Search doSearch={doSearch} />)
+
+    const inputText = 'some text here'
+    const form = screen.getByRole('form')
+    const input = screen.getByRole('searchbox')
+
+    userEvent.type(input, inputText)
+    fireEvent.submit(form)
+
+    expect(doSearch).toHaveBeenCalledWith(inputText)
   })
 })
--- components/Search/Search.tsx
+++ components/Search/Search.tsx
@@ -1,6 +1,27 @@
-const Search = () => {
+import { useCallback, useState } from 'react'
+import type { ChangeEvent, FormEvent } from 'react'
+
+type TSearchProps = {
+  doSearch?: (term: string) => void
+}
+
+const Search = (props: TSearchProps) => {
+  const { doSearch } = props
+
+  const [term, setTerm] = useState('')
+
+  const handleInput = useCallback((event: ChangeEvent<HTMLInputElement>) => {
+    setTerm(event.target.value)
+  }, [])
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+
+    doSearch?.(term)
+  }
+
   return (
-    <div data-testid="search" className="relative mt-6 max-w-lg mx-auto">
+    <form name="search-form" className="relative mt-6 max-w-lg mx-auto" onSubmit={handleSubmit}>
       <span className="absolute inset-y-0 left-0 pl-3 flex items-center">
         <svg className="h-5 w-5 text-gray-500" viewBox="0 0 24 24" fill="none">
           <path
@@ -15,10 +36,12 @@ const Search = () => {
 
       <input
         className="w-full border rounded-md pl-10 pr-4 py-2 focus:border-blue-500 focus:outline-none focus:shadow-outline"
-        type="text"
-        placeholder="Search"
+        type="search"
+        placeholder="Search watch"
+        value={term}
+        onChange={handleInput}
       />
-    </div>
+    </form>
   )
 }
 
